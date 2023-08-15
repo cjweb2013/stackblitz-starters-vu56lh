f021e7df892755ba67569f5ac6f020ab535f6cd8
cjweb2013 <wcorey@alumni.unc.edu>
2023-08-15 07:34:39 -0400

    added service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './feature/home/home.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
   {

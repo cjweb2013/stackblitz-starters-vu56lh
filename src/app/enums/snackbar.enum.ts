a2998bdadb1d3d8e7758b150cb92beb355dd5c72
cjweb2013 <wcorey@alumni.unc.edu>
2023-08-17 15:33:44 -0400

    3 files modified

--- src/app/enums/snackbar.enum.ts
+++ src/app/enums/snackbar.enum.ts
@@ -1,5 +1,5 @@
 export enum ToastClassEnum {
-  warning = 'text-red',
-  success = 'text-green-dark',
-  default = 'text-gray-dark',
+  warning = 'text-red-400',
+  success = 'text-green-400',
+  default = '',
 }
